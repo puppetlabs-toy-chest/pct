db5c71bbc863ddd2e206af04c53ae2596ef5e489
sanfrancrisko <ciaran.mccrisken@puppet.com>
2021-05-27 13:23:29 +0100

    (GH-67) Add installation scripts for PCT
    Adds an `install.[ps1|sh]` script to:
    - Fetch the latest tag from the puppetlabs/pdkgo repo
    - Download the latest PCT archive to `$HOME/.puppetlabs/pct`

---
+++ tools/install.ps1
@@ -0,0 +1,49 @@
+[CmdletBinding()]
+param (
+)
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+$org = 'puppetlabs'
+$repo = 'pdkgo'
+
+$app = 'pct'
+
+$arch = "x86_64"
+$os = 'windows'
+$ext = '.zip'
+
+$ver = (Invoke-RestMethod "https://api.github.com/repos/${org}/${repo}/releases")[0].tag_name
+$file = "${app}_${ver}_${os}_${arch}${ext}"
+$downloadURL = "https://github.com/${org}/${repo}/releases/download/$ver/$file"
+
+$Destination = "~/.puppetlabs/pct"
+$Destination = $PSCmdlet.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Destination)
+
+$tempDir = Join-Path ([System.IO.Path]::GetTempPath()) ([System.IO.Path]::GetRandomFileName())
+$null = New-Item -ItemType Directory -Path $tempDir -Force -ErrorAction SilentlyContinue
+$packagePath = Join-Path -Path $tempDir -ChildPath $file
+
+if (!$PSVersionTable.ContainsKey('PSEdition') -or $PSVersionTable.PSEdition -eq "Desktop") {
+  $oldProgressPreference = $ProgressPreference
+  $ProgressPreference = "SilentlyContinue"
+}
+
+try {
+  Write-Host "Downloading and extracting ${app} ${ver} to ${Destination}"
+  Invoke-WebRequest -Uri $downloadURL -OutFile $packagePath
+}
+finally {
+  if (!$PSVersionTable.ContainsKey('PSEdition') -or $PSVersionTable.PSEdition -eq "Desktop") {
+    $ProgressPreference = $oldProgressPreference
+  }
+}
+
+if (Test-Path -Path $Destination) {
+  Remove-Item -Path $Destination -Force -Recurse
+}
+Expand-Archive -Path $packagePath -DestinationPath $Destination
+
+Write-Host 'Remember to add the pct app to your path:'
+Write-Host "`$env:Path += `"`$env:PATH;${Destination}`""
