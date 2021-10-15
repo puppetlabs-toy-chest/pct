efe066f619708e628d92425658f4070935cf1a74
sanfrancrisko <ciaran.mccrisken@puppet.com>
2021-10-15 13:04:29 +0100

    (MAINT) Add notel options to `install.(ps1|sh)`

--- tools/install.sh
+++ tools/install.sh
@@ -5,6 +5,13 @@ org="puppetlabs"
 repo="pdkgo"
 
 app="pct"
+appPkgName="pct"
+
+noTel="${1:-false}"
+
+if [ "$noTel" = "--no-telemetry" ]; then
+  appPkgName="notel_pct"
+fi
 
 arch="x86_64"
 os="$(uname -s | tr '[:upper:]' '[:lower:]')"
@@ -14,7 +21,7 @@ releases="$(curl -s -H "Accept: application/vnd.github.v3+json" "https://api.git
 
 ver="$(echo "$releases" | grep -oE -m 1 '"tag_name": "(([0-9]+\.)+[0-9]+(-pre+)?)"' | cut -d '"' -f 4 | head -1)"
 
-file="${app}_${ver}_${os}_${arch}${ext}"
+file="${appPkgName}_${ver}_${os}_${arch}${ext}"
 
 downloadURL="https://github.com/${org}/${repo}/releases/download/${ver}/${file}"
 
@@ -22,7 +29,12 @@ destination="${HOME}/.puppetlabs/pct"
 
 [ -d "${destination}" ] || mkdir -p "${destination}"
 
-echo "Downloading and extracting ${app} ${ver} to ${destination}"
+if [ "$noTel" = "--no-telemetry" ]; then
+    echo "Downloading and extracting ${app} ${ver} (TELEMETRY DISABLED VERSION) to ${destination}"
+else
+    echo "Downloading and extracting ${app} ${ver} to ${destination}"
+fi
+
 curl -L -s "${downloadURL}" -o - | tar xz -C "${destination}"
 
 echo 'Remember to add the pct app to your path:'
