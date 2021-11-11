540fbd127b7a9e3609fe31c7bc3dc9d11ccdb525
David Swan <david.swan@puppet.com>
2021-11-11 15:23:23 +0000

    Task
    Practise go by creating a function that retrieves an the information from the following BBC News xml link and gives a list of all the current headlines.
    http://feeds.bbci.co.uk/news/technology/rss.xml

---
+++ cmd/retrieve_xml/retrieve_xml.go
@@ -0,0 +1,2 @@
+// Practise go by creating a function that retrieves an the information from the following BBC News xml link and gives a list of all the current headlines.
+// http://feeds.bbci.co.uk/news/technology/rss.xml
