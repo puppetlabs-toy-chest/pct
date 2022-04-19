7259db21df25e7cf31aa3b5a5f31a7b24c8c73cf
Craig Gumbley <craiggumbley@gmail.com>
2022-04-19 16:28:07 +0100

    (MAINT) Add GOAMD64 eval

--- build.ps1
+++ build.ps1
@@ -14,6 +14,12 @@ $platform = go env GOHOSTOS
 $binPath = Join-Path $PSScriptRoot "dist" "pct_${platform}_${arch}"
 $binPath2 = Join-Path $PSScriptRoot "dist" "notel_pct_${platform}_${arch}"
 
+$amd64 = go env GOAMD64
+if ($amd64) {
+	$binPath = "${binPath}_${amd64}"
+	$binPath2 = "${binPath2}_${amd64}"
+}
+
 switch ($Target) {
   'build' {
     # Set goreleaser to build for current platform only
@@ -26,6 +32,7 @@ switch ($Target) {
     }
     goreleaser build --snapshot --rm-dist --single-target
     git clone -b main --depth 1 --single-branch https://github.com/puppetlabs/baker-round (Join-Path $binPath "templates")
+		Get-ChildItem -Path (Join-Path $binPath "templates")
     Copy-Item (Join-Path $binPath "templates") -Destination (Join-Path $binPath2 "templates") -Recurse
   }
   'quick' {
